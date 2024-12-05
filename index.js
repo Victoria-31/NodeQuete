d3ed55ad2dc754aa2de9d615f5bd5f072f68b896
Victoria Fonteny <victoriafonteny@live.fr>
2024-12-05 17:15:12 +0100

    ajout campus

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import * as cowsay from "cowsay"
 import "dotenv/config";
 
-let output = cowsay.say({ text: `Hello from ${process.env.NAME}` });
+let output = cowsay.say({ text: `Hello I'm ${process.env.NAME} from ${process.env.CAMPUS}` });
 
 console.log(output);
